d8995a70fbb3e77e1b3d5d3b6c115d2db46b18ed
Gurveer1510 <singhgurveer4691@gmail.com>
2026-03-17 15:19:02 +0000

    add middleware

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Gurveer1510/urlshortner/internal/config"
 	"github.com/Gurveer1510/urlshortner/internal/db"
 	"github.com/Gurveer1510/urlshortner/internal/handlers"
+	"github.com/Gurveer1510/urlshortner/internal/middlewares"
 	"github.com/Gurveer1510/urlshortner/internal/persistence"
 	"github.com/Gurveer1510/urlshortner/internal/usecase"
 )
@@ -30,11 +31,13 @@ func main() {
 	uc := usecase.NewUseCase(p)
 	h := handlers.NewHandler(uc, "http://localhost:8080")
 
+	limiter := middlewares.NewRateLimiter(5,10)
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /shorten", h.Shorten)
+	mux.HandleFunc("POST /shorten",  h.Shorten)
 	mux.HandleFunc("GET /{code}", h.Redirect)
 	mux.HandleFunc("GET /stats/{code}", h.GetStats)
 
 	log.Println("Listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", limiter.Middleware(mux)))
 }
---
+++ internal/middlewares/rateLimiter.go
@@ -0,0 +1,94 @@
+package middlewares
+
+import (
+	"net/http"
+	"sync"
+	"time"
+)
+
+type TokenBucket struct {
+	capacity   float64
+	tokens     float64
+	refillRate float64
+	lastRefill time.Time
+	mu         sync.Mutex
+}
+
+func NewTokenBucket(capacity, refillRate float64) *TokenBucket {
+	return &TokenBucket{
+		capacity:   capacity,
+		refillRate: refillRate,
+		tokens:     capacity,
+		lastRefill: time.Now(),
+	}
+}
+
+func (tb *TokenBucket) Allow() bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+
+	tb.refill()
+
+	if tb.tokens > 1 {
+		tb.tokens--
+		return true
+	}
+	return false
+}
+
+func (tb *TokenBucket) refill() {
+	now := time.Now()
+	elapased := now.Sub(tb.lastRefill).Seconds()
+
+	tb.tokens = min(tb.capacity, tb.tokens+elapased*tb.refillRate)
+	tb.lastRefill = now
+}
+
+func min(x, y float64) float64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+type RateLimiter struct {
+	buckets map[string]*TokenBucket
+	mu      sync.Mutex
+	rate    float64
+	cap     float64
+}
+
+func NewRateLimiter(rate, capacity float64) *RateLimiter {
+	return &RateLimiter{
+		buckets: make(map[string]*TokenBucket),
+		rate: rate,
+		cap: capacity,
+	}
+}
+
+func (rl *RateLimiter) getBucket(ip string) *TokenBucket {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if _, ok := rl.buckets[ip]; !ok {
+		rl.buckets[ip] = NewTokenBucket(rl.cap, rl.rate)
+	}
+
+	return rl.buckets[ip]
+}
+
+func (rl *RateLimiter) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) { 
+		ip := r.RemoteAddr
+		bucket := rl.getBucket(ip)
+
+		if !bucket.Allow() {
+			rw.Header().Set("Retry-After", "1")
+			http.Error(rw, "Rate limit exeeded", http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(rw, r)
+	})
+	
+}
