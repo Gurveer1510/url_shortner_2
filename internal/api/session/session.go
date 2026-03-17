be6771542c662ef96815c6460d0334bacedcee46
Gurveer1510 <singhgurveer4691@gmail.com>
2026-03-17 18:31:02 +0000

    add session

---
+++ internal/api/session/session.go
@@ -0,0 +1,72 @@
+package session
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"sync"
+	"time"
+)
+
+type Session struct {
+	UserEmail string
+	CreatedAt time.Time
+	ExpiresAt time.Time
+}
+
+type SessionStore struct {
+	mu       sync.RWMutex
+	sessions map[string]*Session
+}
+
+func NewSessionStore() *SessionStore {
+	s := &SessionStore{sessions: make(map[string]*Session)}
+	go s.cleanUpLoop()
+	return s
+}
+
+func (s *SessionStore) Create(email string) (string, error) {
+	id, err := generateSessionID()
+	if err != nil {
+		return "", err
+	}
+
+	s.mu.Lock()
+	s.sessions[id] = &Session{
+		UserEmail: email,
+		CreatedAt: time.Now(),
+		ExpiresAt: time.Now().Add(7*24*time.Hour),
+	}
+
+	return id, nil
+}
+
+func (s *SessionStore) Get(id string) ( *Session, bool ){
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	session, ok := s.sessions[id]
+	if !ok || time.Now().After(session.ExpiresAt) {
+		return nil, false
+	}
+	return session, true
+}
+
+func (s *SessionStore) cleanUpLoop() {
+	ticker := time.NewTicker(1 * time.Hour)
+	for range ticker.C {
+		s.mu.Lock()
+		for id, session := range s.sessions {
+			if time.Now().After(session.ExpiresAt) {
+				delete(s.sessions, id)
+			}
+		}
+		s.mu.Unlock()
+	}
+}
+
+func generateSessionID() (string, error) {
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
